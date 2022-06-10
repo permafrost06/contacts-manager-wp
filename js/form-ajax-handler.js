a1abd100be9b6bf953691a279e5d67937b84c593
permafrost06 <zahin06@gmail.com>
2022-06-10 16:57:20 +0600

    updated contact form added stylesheet and styles changed form behaviour

--- js/form-ajax-handler.js
+++ js/form-ajax-handler.js
@@ -9,7 +9,10 @@ jQuery(document).ready(function ($) {
     const phone = $("#contact_phone").val();
     const address = $("#contact_address").val();
 
-    const messageEl = $("#message");
+    $("button").prop("disabled", true);
+
+    const successEl = $("#success-message");
+    const errorEl = $("#error-message");
 
     var this2 = this; //use in callback
     $.post(
@@ -25,10 +28,23 @@ jQuery(document).ready(function ($) {
       },
       function (data) {
         if (data.success) {
-          messageEl.text(data.data.message);
-          console.log(data);
-        } else console.log("failure");
+          successEl.text(data.data.message);
+        } else {
+          errorEl.text(data.data.message);
+        }
       }
     );
   });
+
+  $("input").on("input", clearMessage);
+
+  function clearMessage() {
+    const successEl = $("#success-message");
+    const errorEl = $("#error-message");
+
+    $("button").prop("disabled", false);
+
+    successEl.text("");
+    errorEl.text("");
+  }
 });
