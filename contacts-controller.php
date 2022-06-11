aa31df38837c97b31fb3b1383b2efbb2e3c84c20
permafrost06 <zahin06@gmail.com>
2022-06-11 11:52:32 +0600

    cleaned up and simplified

--- contacts-controller.php
+++ contacts-controller.php
@@ -1,47 +1,43 @@
 <?php
 class Contacts_Controller
 {
-  public function add_contact($name, $email, $phone, $address)
+  private $db;
+  private $table_name;
+
+  public function __construct()
   {
     global $wpdb;
-    $table_name = $wpdb->prefix . 'contacts_manager_table';
 
-    $wpdb->insert($table_name, array('name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address));
+    $this->db = $wpdb;
+    $this->table_name = $this->db->prefix . 'contacts_manager_table';
+  }
+
+  public function add_contact($name, $email, $phone, $address)
+  {
+    $this->db->insert($this->this->table_name, array('name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address));
   }
 
   public function get_contact($id)
   {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'contacts_manager_table';
-
-    $data = $wpdb->get_row("SELECT * FROM $table_name WHERE `id` = '$id'", "ARRAY_A");
+    $data = $this->db->get_row("SELECT * FROM $this->table_name WHERE `id` = '$id'", "ARRAY_A");
 
     return $data;
   }
 
   public function get_all_contacts()
   {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'contacts_manager_table';
-
-    $data = $wpdb->get_results("SELECT * FROM $table_name", "ARRAY_A");
+    $data = $this->db->get_results("SELECT * FROM $this->table_name", "ARRAY_A");
 
     return $data;
   }
 
   public function remove_contact($id)
   {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'contacts_manager_table';
-
-    $wpdb->delete($table_name, array('id' => $id));
+    $this->db->delete($this->table_name, array('id' => $id));
   }
 
   public function update_contact($id, $name, $email, $phone, $address)
   {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'contacts_manager_table';
-
-    $wpdb->update($table_name, array('name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address), array('id' => $id));
+    $this->db->update($this->table_name, array('name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address), array('id' => $id));
   }
 }
