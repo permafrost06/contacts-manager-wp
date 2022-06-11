4f6729d844362de219017c5e4014c213c160a53f
permafrost06 <zahin06@gmail.com>
2022-06-11 17:46:57 +0600

    added exceptions added error handling

--- contacts-controller.php
+++ contacts-controller.php
@@ -14,12 +14,19 @@ class Contacts_Controller
 
   public function add_contact($name, $email, $phone, $address)
   {
-    $this->db->insert($this->this->table_name, array('name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address));
+    $response = $this->db->insert($this->this->table_name, array('name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address));
+    if (!$response) {
+      throw new Exception("Could not insert contact");
+    }
   }
 
   public function get_contact($id)
   {
     $data = $this->db->get_row("SELECT * FROM $this->table_name WHERE `id` = '$id'", "ARRAY_A");
+
+    if (!$data) {
+      throw new Exception("Could not get contact. ID might be invalid");
+    }
 
     return $data;
   }
@@ -28,16 +35,40 @@ class Contacts_Controller
   {
     $data = $this->db->get_results("SELECT * FROM $this->table_name", "ARRAY_A");
 
+    if (!$data) {
+      throw new Exception("Could not get contacts");
+    }
+
     return $data;
   }
 
   public function remove_contact($id)
   {
-    $this->db->delete($this->table_name, array('id' => $id));
+    try {
+      $this->get_contact($id);
+
+      $response = $this->db->delete($this->table_name, array('id' => $id));
+
+      if (!$response) {
+        throw new Exception("Could not delete contact. ID might be invalid");
+      }
+    } catch (Exception $error) {
+      throw new Exception("Contact does not exist");
+    }
   }
 
   public function update_contact($id, $name, $email, $phone, $address)
   {
-    $this->db->update($this->table_name, array('name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address), array('id' => $id));
+    try {
+      $this->get_contact($id);
+
+      $response = $this->db->update($this->table_name, array('name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address), array('id' => $id));
+
+      if (!$response) {
+        throw new Exception("Could not update contact");
+      }
+    } catch (Exception $error) {
+      throw new Exception("Contact does not exist");
+    }
   }
 }
