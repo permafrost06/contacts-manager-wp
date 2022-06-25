34af87d1095161bc635ce3a81905c37cb88c3224
permafrost06 <zahin06@gmail.com>
2022-06-25 19:53:46 +0600

    partial restructure moved admin menu (needs reformat) moved shortcode (needs reformat) added composer added autoloading added constants added singleton instance of plugin added activation function added init function

---
+++ includes/Admin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Contacts\Manager;
+
+class Admin
+{
+  function __construct()
+  {
+    new Admin\Menu();
+  }
+}
---
+++ includes/Frontend/Shortcode.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Contacts\Manager\Frontend;
+
+class Shortcode
+{
+  /**
+   * Initializes the class
+   */
+  function __construct()
+  {
+    add_shortcode('contacts-manager', [$this, 'render_shortcode']);
+  }
+
+  /**
+   * Shortcode handler function
+   * 
+   * @param array $atts
+   * @param string content
+   * 
+   * @return string
+   */
+  function render_shortcode($atts = [], $content = null)
+  {
+    return 'Shortcode is working';
+    // $atts = array_change_key_case((array) $atts, CASE_LOWER);
+
+    // $output = '';
+
+    // if (array_key_exists('id', $atts)) {
+    //   $output .= $this->render_contact_card($atts['id']);
+    // } else {
+    //   $output .= $this->render_complete_table();
+    // }
+
+    // if (!is_null($content)) {
+    //   $output .= apply_filters('the_content', $content);
+    // }
+
+    // return $output;
+  }
+
+  function render_contact_card($id)
+  {
+    try {
+      $data = $this->contacts_controller->get_contact($id);
+
+      $output = '<div class="contacts-mgr-box"><div>
+    <p class="field">' . $data['name'] . '</p>
+    <p class="field">' . $data['email'] . '</p>
+    <p class="field">' . $data['phone'] . '</p>
+    <p class="field">' . $data['address'] . '</p>
+    </div></div>';
+    } catch (\Exception $error) {
+      $output = '<div class="contacts-mgr-box"><p>Invalid contact selected</p></div>';
+    }
+
+    return $output;
+  }
+
+  function render_complete_table()
+  {
+    try {
+      $data = $this->contacts_controller->get_all_contacts();
+
+      $output = '<div class="contacts-mgr-box contacts-table"><table class="table table-hover">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Address</th>
+        </tr>
+        </thead>
+      <tbody>';
+
+      foreach ($data as $row) {
+        $output .= '<tr>';
+        foreach ($row as $field) {
+          $output .= '<td>' . $field . '</td>';
+        }
+        $output .= '</tr>';
+      }
+
+      $output .= '</tbody>
+    </table></div>';
+    } catch (\Exception $error) {
+      $output = '<div class="contacts-mgr-box"><p>Could not get table</p></div>';
+    }
+
+    return $output;
+  }
+}
