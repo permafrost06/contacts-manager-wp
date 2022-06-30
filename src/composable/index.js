1397c3a710dc24ac1c407b018a5c3bf3417d36b6
permafrost06 <zahin06@gmail.com>
2022-06-30 18:23:10 +0600

    added default fail function

--- src/composable/index.js
+++ src/composable/index.js
@@ -1,6 +1,19 @@
+import { ElMessage } from "element-plus";
+
 export const sendAJAX = (action, payload = {}, callback, onFail) => {
   const prefix = "cm";
   payload.action = `${prefix}_${action}`;
+  payload._ajax_nonce = contactsMgrAdmin.nonce;
+
+  if (!onFail) {
+    onFail = () => {
+      ElMessage({
+        message: "AJAX Request Failed",
+        type: "error",
+      });
+      console.log("AJAX Request Failed");
+    };
+  }
 
   window.jQuery
     .post(contactsMgrAdmin.ajax_url, payload, (data) => {
