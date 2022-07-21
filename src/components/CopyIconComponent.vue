d15da74c12af63e85f6e0a9cd835b155b8bdc003
permafrost06 <zahin06@gmail.com>
2022-07-21 18:19:43 +0600

    Added shortcode column to table changed min width of columns changed pre tags to code fixed code styles added copyicon added copy shortcode button
    closes #24

---
+++ src/components/CopyIconComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 64 64"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M42.4768 16.6192V13C42.4768 10.7909 40.686 9 38.4768 9H16C13.7909 9 12 10.7909 12 13V44C12 46.2091 13.7909 48 16 48H20.7815"
+      stroke="black"
+    />
+    <rect
+      x="21.0232"
+      y="16.4735"
+      width="29.4768"
+      height="38"
+      rx="3.5"
+      stroke="black"
+    />
+  </svg>
+</template>
--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -4,6 +4,7 @@ import { useRouter } from "vue-router";
 import { getAJAX, postAJAX } from "../composable";
 import { ElMessage } from "element-plus";
 import "element-plus/es/components/message/style/css";
+import CopyIcon from "../components/CopyIconComponent.vue";
 
 const contacts = ref([]);
 const deleteID = ref("");
@@ -57,36 +58,52 @@ const confirmDelete = () => {
   });
 
   dialogVisible.value = false;
-}
+};
 
 const currentPage = ref(1);
 const pageSize = ref(10);
 
 const contactsPage = computed(() => {
-  return contacts.value.slice(pageSize.value * (currentPage.value - 1), pageSize.value * currentPage.value);
-})
+  return contacts.value.slice(
+    pageSize.value * (currentPage.value - 1),
+    pageSize.value * currentPage.value
+  );
+});
+
+const copyShortcode = async (id) => {
+  const shortcode = `[contacts-manager id="${id}"]`;
+  await navigator.clipboard.writeText(shortcode);
+
+  ElMessage({
+    message: "Shortcode copied to clipboard",
+    type: "success",
+  });
+};
 </script>
 
 <template>
   <el-row>
     <p class="message">
-      Use shortcode <pre>[contacts-manager]</pre> to render contacts table. Specify ID with
-      <pre>id</pre> attribute to render a specific contact card. Example:
-      <pre>[contacts-manager id="7"]</pre> to render contact card with ID 7.
+      Use shortcode <code>[contacts-manager]</code> to render contacts table.
+      Specify ID with <code>id</code> attribute to render a specific contact
+      card. Example: <code>[contacts-manager id="7"]</code> to render contact
+      card with ID 7.
     </p>
   </el-row>
   <el-row>
     <p class="message">
-      Use shortcode <pre>[contact-form]</pre> to render a contact form where visitors can
-      add new contacts.
+      Use shortcode <code>[contact-form]</code> to render a contact form where
+      visitors can add new contacts.
     </p>
   </el-row>
   <el-row justify="space-between">
     <el-col class="flex-align-center" :span="9">
       <h2 class="inline space-after">Contacts List</h2>
-      <el-button type="primary" @click="handleAddNew">Add new contact</el-button>
+      <el-button type="primary" @click="handleAddNew"
+        >Add new contact</el-button
+      >
     </el-col>
-      <el-pagination
+    <el-pagination
       v-model:currentPage="currentPage"
       v-model:page-size="pageSize"
       :page-sizes="[10, 20, 30, 40]"
@@ -99,23 +116,35 @@ const contactsPage = computed(() => {
     <el-col>
       <el-table v-loading="loading" :data="contactsPage" style="width: 100%">
         <el-table-column prop="id" label="id" width="40" />
-        <el-table-column prop="name" label="Name" width="200" />
-        <el-table-column prop="email" label="Email" />
-        <el-table-column prop="phone" label="Phone no." />
-        <el-table-column prop="address" label="Address" />
-        <el-table-column label="Operations">
+        <el-table-column prop="name" label="Name" min-width="16" />
+        <el-table-column prop="email" label="Email" min-width="18" />
+        <el-table-column prop="phone" label="Phone no." min-width="13" />
+        <el-table-column prop="address" label="Address" min-width="14" />
+        <el-table-column label="Operations" min-width="13">
           <template #default="{ row }">
             <el-button size="small" @click="handleEdit(row.id)">Edit</el-button>
-            <el-button size="small" type="danger" @click="handleDelete(row.id)"
-              >Delete</el-button
+            <el-button size="small" type="danger" @click="handleDelete(row.id)">
+              Delete
+            </el-button>
+          </template>
+        </el-table-column>
+        <el-table-column label="Shortcode" min-width="22">
+          <template #default="{ row }">
+            <code>[contacts-manager id="{{ row.id }}"]</code>
+            <el-button
+              class="no-padding"
+              size="small"
+              @click="copyShortcode(row.id)"
             >
+              <CopyIcon class="svg-button" />
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-col>
   </el-row>
   <el-row justify="end">
-      <el-pagination
+    <el-pagination
       v-model:currentPage="currentPage"
       v-model:page-size="pageSize"
       :page-sizes="[10, 20, 30, 40]"
@@ -140,14 +169,6 @@ const contactsPage = computed(() => {
   margin-right: 1rem;
 }
 
-.message {
-  line-height: .1rem;
-}
-
-.message pre {
-  display: inline-block;
-}
-
 .inline {
   display: inline-block;
 }
@@ -155,5 +176,9 @@ const contactsPage = computed(() => {
 .flex-align-center {
   display: flex;
   align-items: center;
+}
+
+.no-padding {
+  padding: 0;
 }
 </style>
