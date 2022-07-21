aa4762e7552d698c58b3d65ab88e36592be0f4e9
permafrost06 <zahin06@gmail.com>
2022-07-21 16:23:43 +0600

    translation not necessary

--- includes/Frontend/views/contact-card.php
+++ includes/Frontend/views/contact-card.php
@@ -1,8 +1,8 @@
   <div class="contacts-mgr-box contact-card">
     <div>
-      <p class="field"><?php esc_html_e($contact->name, "contacts-manager") ?></p>
-      <p class="field"><?php esc_html_e($contact->email, "contacts-manager") ?></p>
-      <p class="field"><?php esc_html_e($contact->phone, "contacts-manager") ?></p>
-      <p class="field"><?php esc_html_e($contact->address, "contacts-manager") ?></p>
+      <p class="field"><?php esc_html_e($contact->name) ?></p>
+      <p class="field"><?php esc_html_e($contact->email) ?></p>
+      <p class="field"><?php esc_html_e($contact->phone) ?></p>
+      <p class="field"><?php esc_html_e($contact->address) ?></p>
     </div>
   </div>
