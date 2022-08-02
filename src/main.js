936037f8079fc23915792de911ad5524034e92af
permafrost06 <zahin06@gmail.com>
2022-08-02 20:00:18 +0600

    removed pinia closes #65

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 
-const app = createApp(App).use(createPinia());
+const app = createApp(App);
 
 app.use(router);
 
