b56b38a4b0fd0863f7fd4e5e77a4e81aebc8b775
Zahin Azmayeen <zahin06@gmail.com>
2023-11-24 21:20:15 +0600

    remove vendor from gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'permafrost/contacts-manager',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4945653f4c5f9e01599c51abf7a4df153dc65174',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'permafrost/contacts-manager' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4945653f4c5f9e01599c51abf7a4df153dc65174',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
